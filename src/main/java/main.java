11ee2ea7cfd7acbe5468c4c65b6defd155cfcf2f
Sheldon Hall <sheldon@ontoit.io>
2016-05-24 15:46:15 +0100

    about to update to multiple relation types

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,22 +1,23 @@
+import io.mindmaps.core.dao.MindmapsGraph;
 import io.mindmaps.core.dao.MindmapsGraphFactory;
+import io.mindmaps.core.structure.types.ConceptInstance;
 import io.mindmaps.graph.config.TitanGraphFactory;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.Map.Entry;
 
 /**
  *
  */
 
 public class main {
-    Set<String> postAssertions = ConcurrentHashMap.newKeySet();
 
     public static void main(String [ ] args) throws InterruptedException {
-        String graphConf = "/opt/mindmaps/resources/conf/titan-cassandra-es-batch.properties";
+        String graphConf = "/opt/mindmaps/resources/conf/titan-cassandra-es.properties";
 
         // get iid maps in memory
         LoadExtractedData data = new LoadExtractedData();
@@ -29,16 +30,55 @@ public class main {
         // get graph factory
         MindmapsGraphFactory mg = new TitanGraphFactory();
 
-        // perform fix of all keywords
+        // collect iterators for round robin load
+        MindmapsGraph transaction = mg.buildTransaction(graphConf);
+        Graph g = transaction.getGraph();
+        Map<String,GraphTraversal<Vertex, Vertex>> iterators = new HashMap<>();
+//        System.out.println(data.conceptsIID.get("keyword").size());
+//        System.out.println(data.conceptsIID.get("keyword"));
         data.conceptsIID.get("keyword").forEach((k,v)->
             v.forEach(iid -> {
-                if (fix.resolutionIIDMap.containsKey(iid)) {
-                    UpdateKeyword.execute(iid, fix.resolutionIIDMap.get(iid), mg, graphConf);
-                } else {
-                    UpdateKeyword.execute(iid, iid, mg, graphConf);
+                // confirm that we have a keyword
+//                System.out.println(iid);
+                ConceptInstance potentialKeyword = transaction.getConceptInstanceByItemIdentifier(iid);
+                if (potentialKeyword != null) {
+                    if (!potentialKeyword.getType()
+                            .equals("http://mindmaps.io/keyword")) {
+                        throw new RuntimeException();
+                    }
+
+                    // collect iterators
+                    if (fix.resolutionIIDMap.containsKey(iid)) {
+                        iterators.put(iid, g.traversal().V().
+                                has("ITEM_IDENTIFIER", fix.resolutionIIDMap.get(iid)).
+                                outE("RELATION").
+                                has("TO_TYPE", "http://mindmaps.io/movie").otherV());
+                    } else {
+                        iterators.put(iid, g.traversal().V().
+                                has("ITEM_IDENTIFIER", iid).
+                                outE("RELATION").
+                                has("TO_TYPE", "http://mindmaps.io/movie").otherV());
+                    }
                 }
             })
         );
+
+        // perform fix of all keywords
+        int i = 0;
+        boolean hasNext = true;
+        while (hasNext) {
+            i++;
+            hasNext = false;
+            for (Entry<String,GraphTraversal<Vertex, Vertex>> entry : iterators.entrySet()) {
+                if (entry.getValue().hasNext()) {
+                    UpdateKeyword.execute(entry.getKey(),
+                            entry.getValue().next().value("ITEM_IDENTIFIER"));
+                    hasNext |= entry.getValue().hasNext();
+                }
+                //TODO: remove the iterator once it is empty
+            }
+            System.out.println("Round robin completed: "+String.valueOf(i));
+        }
 
         System.exit(0);
     }
--- src/main/java/updateKeyword.java
+++ src/main/java/updateKeyword.java
@@ -1,13 +1,4 @@
-import io.mindmaps.core.dao.MindmapsGraph;
 import io.mindmaps.core.dao.MindmapsGraphFactory;
-import io.mindmaps.core.dao.MindmapsGraphImpl;
-import io.mindmaps.core.structure.types.ConceptInstance;
-import io.mindmaps.core.structure.types.RelationType;
-import io.mindmaps.core.structure.types.RoleType;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
-import org.apache.tinkerpop.gremlin.structure.Graph;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -16,10 +7,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.NoRouteToHostException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
  *
@@ -30,90 +19,75 @@ public class UpdateKeyword {
      * This function takes movies attached to the source and creates the mm:related-concept relationship between those
      * movies and the concept indicated by the target.
      *
-     * @param source an iid for a keyword
-     * @param target and iid for a concept instance
-     * @param config a titan config file
+     * @param concept an iid for a concept instance to be attached to a movie
+     * @param movie and iid for a concept instance that is a movie
      */
-    public static void execute(String source, String target, MindmapsGraphFactory mg, String config){
-
-        // get graph transaction
-        MindmapsGraph transaction = mg.buildTransaction(config);
-        Graph g = transaction.getGraph();
-
-        // fetch iterator of movies connected to keyword
-        GraphTraversal<Vertex, Vertex> movies = g.traversal().V().
-                has("ITEM_IDENTIFIER", source).
-                outE("RELATION").
-                has("TO_TYPE", "http://mindmaps.io/movie").otherV();
+    public static void execute(String concept, String movie){
 
         // insert relations of type keyword-concept
-        movies.forEachRemaining(v -> {
-            MindmapsGraph assertionTransaction = mg.buildTransaction(config);
+        try {
+            // create payload
+            JsonObject post = createPost(movie, concept);
 
-            // get source and target concepts to check for errors in input
-            if (!assertionTransaction.getConceptInstanceByItemIdentifier(source).getType()
-                    .equals("http://mindmaps.io/keyword")) {throw new RuntimeException();}
-//            if (assertionTransaction.getConceptInstanceByItemIdentifier(target)
-//                    .equals(null)) {throw new RuntimeException();}
-
+            // prepare post
+            // TODO deploy needs port 80
+            URL url = new URL("http://localhost:8080/transaction");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestMethod("POST");
             try {
-                // prepare post
-                URL url = new URL("http://localhost:8080/transaction");
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setDoInput(true);
-                con.setDoOutput(true);
-                con.setRequestProperty("Content-Type", "application/json");
-                con.setRequestProperty("Accept", "application/json");
-                con.setRequestMethod("POST");
                 OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
-
-                // create payload
-                JsonObject post = createPost(v.value("ITEM_IDENTIFIER"), target);
 
                 // send
                 wr.write(post.toString());
                 wr.flush();
-
-                // display response
-
-                StringBuilder sb = new StringBuilder();
-                int HttpResult = con.getResponseCode();
-                if (HttpResult == HttpURLConnection.HTTP_OK) {
-                    BufferedReader br = new BufferedReader(
-                            new InputStreamReader(con.getInputStream(), "utf-8"));
-                    String line = null;
-                    while ((line = br.readLine()) != null) {
-                        sb.append(line + "\n");
-                    }
-                    br.close();
-                    System.out.println("" + sb.toString());
-                } else {
-                    System.out.println(con.getResponseMessage());
-                }
-
-            } catch (IOException e) {
+            } catch (NoRouteToHostException e) {
                 e.printStackTrace();
+                System.out.println("Traffic too high - Failed to commit post: "+post);
             }
-        });
 
-        // close transaction
-        transaction.commit();
+            // display response
+
+            StringBuilder sb = new StringBuilder();
+            int HttpResult = con.getResponseCode();
+            if (HttpResult == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(con.getInputStream(), "utf-8"));
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                br.close();
+                System.out.println("" + sb.toString());
+            } else {
+                System.out.println(con.getResponseMessage());
+            }
+
+            // close connection
+            con.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static JsonObject createPost(String movie, String concept) {
         return Json.createObjectBuilder()
-                .add("add",Json.createObjectBuilder()
-                    .add("relationships",Json.createObjectBuilder()
-                        .add("keyword-concept", Json.createArrayBuilder()
-                            .add(Json.createObjectBuilder()
-                                .add("roles", Json.createObjectBuilder()
-                                    .add("related-concept",concept)
-                                    .add("movie-with-keyword",movie)
-                                )
+            .add("add", Json.createObjectBuilder()
+                .add("relationships", Json.createObjectBuilder()
+                    .add("keyword-concept", Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                            .add("roles", Json.createObjectBuilder()
+                                .add("related-concept", concept)
+                                .add("movie-with-keyword", movie)
                             )
                         )
                     )
                 )
-                .build();
+            )
+            .build();
     }
 }
